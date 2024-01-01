6f4526104f458fe50833d1d2835ef58c758362f7
yhzzhk <yhzzhk@163.com>
2024-01-02 04:05:09 +0800

    modified:   p2p/neo4j/neo4j.go

--- p2p/neo4j/neo4j.go
+++ p2p/neo4j/neo4j.go
@@ -8,18 +8,18 @@ import (
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
 
-// const (
-//
-//	uri      = "bolt://localhost:7687"
-//	username = "neo4j"
-//	password = "11111111"
-//
-// )
-var (
-	uri      = os.Getenv("NEO4J_URI")
-	username = os.Getenv("NEO4J_USERNAME")
-	password = os.Getenv("NEO4J_PASSWORD")
+const (
+
+	uri      = "bolt://localhost:7687"
+	username = "neo4j"
+	password = "testpassword"
+
 )
+// var (
+// 	uri      = os.Getenv("NEO4J_URI")
+// 	username = os.Getenv("NEO4J_USERNAME")
+// 	password = os.Getenv("NEO4J_PASSWORD")
+// )
 
 type cqlconnection struct {
 	uri      string
